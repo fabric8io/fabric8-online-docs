44a28dc4bb5631a9939adcc1640800316c0f0297
Robert Krátký <rkratky@redhat.com>
2017-11-10 20:49:22 +0100

    Add PDF building to book build scripts.

--- docs/e2e_workshop/build_guide.sh
+++ docs/e2e_workshop/build_guide.sh
@@ -1,9 +1,10 @@
 
 #Name of the html output file
-GUIDE_HTML_NAME=openshift-io_end_to_end_workshop.html
+GUIDE_HTML_NAME=openshift-io_end_to_end_workshop
 OUTPUT_DIR=../../html
 
 rm -r $OUTPUT_DIR/images/
 cp -r topics/images/ $OUTPUT_DIR/images/
 
-asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME
+asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME.html
+asciidoctor -r asciidoctor-pdf -a imagesdir="topics/images" -b pdf master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME.pdf
--- docs/user_guide/build_guide.sh
+++ docs/user_guide/build_guide.sh
@@ -1,9 +1,10 @@
 
 #Name of the html output file
-GUIDE_HTML_NAME=openshift-io_user_guide.html
+GUIDE_HTML_NAME=openshift-io_user_guide
 OUTPUT_DIR=../../html
 
 rm -r $OUTPUT_DIR/images/
 cp -r topics/images/ $OUTPUT_DIR/images/
 
-asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME
+asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME.html
+asciidoctor -r asciidoctor-pdf -a imagesdir="topics/images" -b pdf master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME.pdf
