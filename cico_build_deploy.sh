958d7a199619bd80adb741dc9ac095bda769406b
Jaime Melis <jmelis@redhat.com>
2018-04-25 17:47:30 +0200

    Build on RHEL and move from caddy to nginx
    Enables build on RHEL if TARGET=rhel:
    
    - The build scripts will be executed twice on the same machine, once for
      CentOS and one for RHEL
    - `docker login` is required to run `docker build`
    - The image target is different if TARGET=rhel
    - There is a separate Dockerfile for the RHEL build
    
    Additionally, it replaces caddy with nginx as the static web server, to match
    other services in OSIO.

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -4,24 +4,35 @@ GENERATOR_DOCKER_HUB_USERNAME=openshiftioadmin
 REGISTRY_URI="push.registry.devshift.net"
 REGISTRY_NS="fabric8io"
 REGISTRY_IMAGE="fabric8-online-docs"
-REGISTRY_URL=${REGISTRY_URI}/${REGISTRY_NS}/${REGISTRY_IMAGE}
 BUILDER_IMAGE="docs-builder"
 BUILDER_CONT="docs-builder-container"
 DEPLOY_IMAGE="docs-deploy"
 DEPLOY_CONT="docs-deploy-container"
 
+if [ "$TARGET" = "rhel" ]; then
+    REGISTRY_URL=${REGISTRY_URI}/osio-prod/${REGISTRY_NS}/${REGISTRY_IMAGE}
+    DOCKERFILE_DEPLOY="Dockerfile.deploy.rhel"
+else
+    REGISTRY_URL=${REGISTRY_URI}/${REGISTRY_NS}/${REGISTRY_IMAGE}
+    DOCKERFILE_DEPLOY="Dockerfile.deploy"
+fi
+
 TARGET_DIR="html"
 
-function tag_push() {
-    TARGET_IMAGE=$1
-    USERNAME=$2
-    PASSWORD=$3
-    REGISTRY=$4
+function docker_login() {
+    local USERNAME=$1
+    local PASSWORD=$2
+    local REGISTRY=$3
 
-    docker tag ${DEPLOY_IMAGE} ${TARGET_IMAGE}
     if [ -n "${USERNAME}" ] && [ -n "${PASSWORD}" ]; then
         docker login -u ${USERNAME} -p ${PASSWORD} ${REGISTRY}
     fi
+}
+
+function tag_push() {
+    local TARGET_IMAGE=$1
+
+    docker tag ${DEPLOY_IMAGE} ${TARGET_IMAGE}
     docker push ${TARGET_IMAGE}
 }
 
@@ -33,10 +44,7 @@ if [ -z $CICO_LOCAL ]; then
     [ -f inherit-env ] && . inherit-env
 
     # We need to disable selinux for now, XXX
-    /usr/sbin/setenforce 0
-
-    # Get all the deps in
-    yum -y install docker make git
+    /usr/sbin/setenforce 0 || :
 
     # Get all the deps in
     yum -y install docker make git
@@ -51,7 +59,7 @@ docker ps -a | grep -q ${BUILDER_CONT} && docker rm ${BUILDER_CONT}
 if [ -d $TARGET_DIR ]; then rm -rf ${TARGET_DIR}/; fi
 
 #CREATE A FRESH DIR FOR BUILT DOCS TO BE PUT INTO
-mkdir -m 777 ${TARGET_DIR}
+mkdir -pm 777 ${TARGET_DIR}
 
 #BUILD BUILDER IMAGE
 docker build -t ${BUILDER_IMAGE} -f Dockerfile.build .
@@ -59,14 +67,19 @@ docker build -t ${BUILDER_IMAGE} -f Dockerfile.build .
 #RUN BUILDER CONTAINER ONCE, THUS BUILDING THE DOCS
 docker run --rm --name=${BUILDER_CONT} -it --volume=$(pwd)/${TARGET_DIR}:/${TARGET_DIR}:Z ${BUILDER_IMAGE}
 
+#LOGIN IS REQUIRED FOR BUILDING ON RHEL
+if [ -z "$CICO_LOCAL" ]; then
+  docker_login "${DEVSHIFT_USERNAME}" "${DEVSHIFT_PASSWORD}" "${REGISTRY_URI}"
+fi
+
 #BUILD DEPLOY IMAGE
-docker build -t ${DEPLOY_IMAGE} -f Dockerfile.deploy .
+docker build -t ${DEPLOY_IMAGE} -f "${DOCKERFILE_DEPLOY}" .
 
 #PUSH
 if [ -z $CICO_LOCAL ]; then
     TAG=$(echo $GIT_COMMIT | cut -c1-${DEVSHIFT_TAG_LEN})
-    tag_push "${REGISTRY_URL}:${TAG}" ${DEVSHIFT_USERNAME} ${DEVSHIFT_PASSWORD} ${REGISTRY_URI}
-    tag_push "${REGISTRY_URL}:latest" ${DEVSHIFT_USERNAME} ${DEVSHIFT_PASSWORD} ${REGISTRY_URI}
+    tag_push "${REGISTRY_URL}:${TAG}"
+    tag_push "${REGISTRY_URL}:latest"
 fi
 
 #SERVE DOCS WHEN ON LOCAL
